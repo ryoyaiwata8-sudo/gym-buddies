c5abecbddc30cbb1bce09644eff996aca9bc813f
Ryoya Iwata <shared-ai@vexum-ai.com>
2025-11-06 17:00:00 +0900

    Remove experimental optimizeCss to fix Vercel build error

--- next.config.js
+++ next.config.js
@@ -21,9 +21,6 @@ const withPWA = require('next-pwa')({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 module.exports = withPWA(nextConfig)
